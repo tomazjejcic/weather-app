2f2b7d98bb167e66d2637b8acd0a04b18d6d23d7
Tomaz Jejcic <tomazjejcic@gmail.com>
2017-08-27 15:11:11 +0800

    initial commit

---
+++ src/app/map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'map-component',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent {
+
+}
