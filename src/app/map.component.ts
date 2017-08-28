12dbb64b8b8abe6a7ed4a75488fafe976b747e5c
Tomaz Jejcic <tomazjejcic@gmail.com>
2017-08-28 21:07:34 +0800

    add(map) marker location, user geolocation

--- src/app/map.component.ts
+++ src/app/map.component.ts
@@ -1,12 +1,85 @@
 import { Component } from '@angular/core';
+// import { ModalService } from '../_services/index';
+
+// just an interface for type safety.
+// interface Marker {
+// 	lat: number;
+// 	lng: number;
+// 	label?: string;
+// 	draggable: boolean;
+// }
 
 @Component({
-  selector: 'map-component',
+  selector: 'app-map-component',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-    title: string = 'map selector';
-    lat: number = 51.678418;
-    lng: number = 7.809007;
+    title = 'map selector';
+    lat = 51.678418;
+    lng = 7.809007;
+
+    // lat1: number = 51.678418;
+    // lng1: number = 7.809007;
+
+    // zoom: number = 8;
+
+    // markers: marker[] = [
+
+    // ]
+    constructor(
+
+    ) {
+        this.getLocation();
+    }
+
+    getLocation() {
+        console.log('WHAT RUNNING', navigator.geolocation.getCurrentPosition);
+        // navigator.geolocation.getCurrentPosition(success, error);
+
+        let startPos;
+        const nudge = document.getElementById('nudge');
+
+        const showNudgeBanner = function() {
+            nudge.style.display = 'block';
+        };
+
+        const hideNudgeBanner = function() {
+            nudge.style.display = 'none';
+        };
+
+        const nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+
+
+        const geoSuccess = function(position) {
+            console.log('POSITTION', position);
+            hideNudgeBanner();
+            // We have the location, don't display banner
+            clearTimeout(nudgeTimeoutId);
+
+            // Do magic with location
+            startPos = position;
+            document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+            document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+        };
+        const geoError = function(error) {
+            console.log('geoError: ', error);
+            switch (error.code) {
+            case error.TIMEOUT:
+                // The user didn't accept the callout
+                showNudgeBanner();
+                break;
+            }
+        };
+
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+
+    mapClicked(event) {
+        // console.log('LLLL', event);
+        console.log('lat', event.coords.lat);
+        console.log('lng', event.coords.lng);
+        this.lat = event.coords.lat;
+        this.lng = event.coords.lng
+    }
 }
