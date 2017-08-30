4d22d99609f304a4d8048d1a6d0b62d2fa7ffe71
Tomaz Jejcic <tomazjejcic@gmail.com>
2017-08-30 16:58:14 +0800

    build(map) initial location, move marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,89 @@ import { LocationModalComponent } from './location-modal/location-modal.componen
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Weather App';
 
     @ViewChild('locationModal') locationModal: LocationModalComponent;
+
+    // Variables
+    private title = 'Weather App';
+    private locationObject = {
+        lat: null,
+        lng: null,
+        zoom: 2
+    }
 
     constructor(
 
     ) {
 
+      this.confirmLocationSupport();
     }
 
-    showModal() {
-      this.locationModal.show()
+    confirmLocationSupport() {
+
+      if (navigator.geolocation) {
+          // geolocation is supported
+          this.getLocation();
+      } else {
+          // geolocation is not supported for this browser
+          console.log('geolocation is not supported')
+      }
     }
+
+    getLocation() {
+
+        let startPos;
+        // const nudge = document.getElementById('nudge');
+
+        // const showNudgeBanner = function() {
+        //     nudge.style.display = 'block';
+        // };
+
+        // const hideNudgeBanner = function() {
+        //     nudge.style.display = 'none';
+        // };
+
+        // const nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+
+
+        const geoSuccess = function(position) {
+
+            console.log(' geo POSITTION', position);
+            startPos = position;
+            locationAllowed();
+            // hideNudgeBanner();
+            // We have the location, don't display banner
+            // clearTimeout(nudgeTimeoutId);
+
+            // document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+            // document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+        };
+        const geoError = function(error) {
+
+            console.log('geoError: ', error);
+            switch (error.code) {
+            case error.TIMEOUT:
+                // The user didn't accept the callout
+                // showNudgeBanner();
+                break;
+            }
+        };
+
+        const locationAllowed = (() => {
+
+            const allowed = {
+                lat: startPos.coords.latitude,
+                lng: startPos.coords.longitude,
+                zoom: 2
+            }
+            this.locationObject = allowed
+        })
+
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+
+    // TODO: future search location
+    // showModal() {
+    //   this.locationModal.show()
+    // }
 }
--- src/app/map.component.ts
+++ src/app/map.component.ts
@@ -1,82 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 // import { ModalService } from '../_services/index';
-
-// just an interface for type safety.
-// interface Marker {
-// 	lat: number;
-// 	lng: number;
-// 	label?: string;
-// 	draggable: boolean;
-// }
 
 @Component({
   selector: 'app-map-component',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
-    title = 'map selector';
-    lat = 51.678418;
-    lng = 7.809007;
+export class MapComponent implements OnChanges {
 
-    // lat1: number = 51.678418;
-    // lng1: number = 7.809007;
+    // @Input //Bindings
+    @Input() userLocation: any;
 
-    // zoom: number = 8;
+    // Variables
+    private title = 'map selector';
+    private lat = null;
+    private lng = null;
+    private zoom = 2;
 
-    // markers: marker[] = [
-
-    // ]
     constructor(
 
     ) {
-        this.getLocation();
+
     }
 
-    getLocation() {
-        console.log('WHAT RUNNING', navigator.geolocation.getCurrentPosition);
-        // navigator.geolocation.getCurrentPosition(success, error);
+    ngOnChanges(changes: SimpleChanges) {
 
-        let startPos;
-        const nudge = document.getElementById('nudge');
+        console.log('User Allows Location', this.userLocation);
+        if (this.userLocation) {
+            this.lat = this.userLocation.lat;
+            this.lng = this.userLocation.lng;
+            this.zoom = this.userLocation.zoom;
+        }
 
-        const showNudgeBanner = function() {
-            nudge.style.display = 'block';
-        };
-
-        const hideNudgeBanner = function() {
-            nudge.style.display = 'none';
-        };
-
-        const nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
-
-
-        const geoSuccess = function(position) {
-            console.log('POSITTION', position);
-            hideNudgeBanner();
-            // We have the location, don't display banner
-            clearTimeout(nudgeTimeoutId);
-
-            // Do magic with location
-            startPos = position;
-            document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-            document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-        };
-        const geoError = function(error) {
-            console.log('geoError: ', error);
-            switch (error.code) {
-            case error.TIMEOUT:
-                // The user didn't accept the callout
-                showNudgeBanner();
-                break;
-            }
-        };
-
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
     }
 
     mapClicked(event) {
-        // console.log('LLLL', event);
+
+        console.log('\n Marker Location Changed', this.userLocation);
         console.log('lat', event.coords.lat);
         console.log('lng', event.coords.lng);
         this.lat = event.coords.lat;
